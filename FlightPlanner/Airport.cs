531f6b92b4c471b77e28712c44789af2ce5609f7
Gatiskk <gatisk19@inbox.lv>
2022-09-28 22:48:22 +0300

    First

---
+++ FlightPlanner/Airport.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FlightPlanner
+{
+    public class Airport
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        [JsonPropertyName("airport")]
+        public string AirportName { get; set; }
+
+        public bool Equals(Airport airport)
+        {
+            var CountryCheck = this.Country == airport.Country;
+            var CityCheck = this.City == airport.City;
+            var AirportNameCheck = this.AirportName == airport.AirportName;
+            return CountryCheck && CityCheck && AirportNameCheck;
+        }
+    }
+}
---
+++ FlightPlanner/Controllers/AdminApiController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("admin-api")]
+    [ApiController, Authorize]
+    public class AdminApiController : ControllerBase
+    {
+        [Route("flights/{id}")]
+        [HttpGet]
+        public IActionResult GetFlight(int id)
+        {
+            var flight = FlightStorage.GetFlight(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            return Ok(flight);
+        }
+
+        [Route("flights")]
+        [HttpPut]
+        public IActionResult PutFlight(Flight flight)
+        {
+            if (flight == null)
+            {
+                return NoContent();
+            }
+
+            if (!FlightStorage.ValidFormat(flight))
+            {
+                return BadRequest();
+            }
+
+            if (FlightStorage.HasSameAirport(flight))
+            {
+                return BadRequest();
+            }
+
+            if (FlightStorage.CheckFlightAndAirport(flight))
+            {
+                return Conflict();
+            }
+            
+            flight = FlightStorage.AddFlight(flight);
+            return Created("", flight);
+        }
+
+        [Route("flights/{id}")]
+        [HttpDelete]
+        public IActionResult FlightDelete(int id)
+        {
+            FlightStorage.FlightDelete(id);
+            return Ok();
+        }
+    }
+}
